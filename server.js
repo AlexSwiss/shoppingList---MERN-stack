8f57d606586d386e1a078c783b5978b7562ce245
AlexSwiss <39837597+AlexSwiss@users.noreply.github.com>
2019-05-03 02:47:55 +0100

    version 1.0.0

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-require("dotenv").config();
 
 const items = require('./routes/api/items');
 
@@ -16,43 +15,24 @@ const db = require('./config/keys').mongoURI;
 
 // Connect to Mongo
 mongoose
-  .connect(db, {useNewUrlParser: true}) // Adding new mongo url parser
+  .connect(db) // Adding new mongo url parser
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
 // Use Routes
 app.use('/api/items', items);
 
-app.use(function (req, res, next) {
-    
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-      
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-      
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-      
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
-      
-        // Pass to next layer of middleware
-        next();
-      });
+//serve static asste if in production
+if(process.env.NODE_ENV == 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
 
-      // ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 
-// Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+}
 
-app.listen(process.env.PORT || 8080, function() {
-  console.log('Express server is up and running!');
-});
-
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
